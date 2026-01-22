e7e709a9c7354d720453cb317ddaba4fb13f7ddb
capSaymon <szymonpietruszka@onet.pl>
2026-01-22 22:35:24 +0100

    modyfikacja blog aby korzytsała z serwisu

--- src/app/components/blog/blog.ts
+++ src/app/components/blog/blog.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { DataService } from '../../services/data';
+import { BlogItem } from '../blog-item/blog-item';
 
 @Component({
-  selector: 'blog',
-  imports: [],
-  templateUrl: './blog.html',
-  styleUrl: './blog.scss',
+ selector: 'blog',
+ standalone: true,
+ imports: [BlogItem, CommonModule],
+ providers: [DataService],
+ templateUrl: './blog.html',
+ styleUrl: './blog.css'
 })
-export class Blog {
+export class BlogComponent implements OnInit{
+ public items: any;
+
+ constructor(private service: DataService) {
+ }
+
+ ngOnInit() {
+   this.items = this.service.getAll();
+ }
 
 }
