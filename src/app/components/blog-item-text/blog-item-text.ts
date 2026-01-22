5242a7c81f0365fde03f94efb0f3f8c010ad58f2
capSaymon <szymonpietruszka@onet.pl>
2026-01-22 22:15:05 +0100

    komponent blog-item-text

---
+++ src/app/components/blog-item-text/blog-item-text.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-item-text',
+  imports: [],
+  templateUrl: './blog-item-text.html',
+  styleUrl: './blog-item-text.scss',
+})
+export class BlogItemText {
+
+}
