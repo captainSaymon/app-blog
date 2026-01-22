7f9db2395d2a085a8e074cb2ca6a2b9f4eb8740e
capSaymon <szymonpietruszka@onet.pl>
2026-01-22 22:15:31 +0100

    komponent blog-item-image

---
+++ src/app/components/blog-item-image/blog-item-image.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-item-image',
+  imports: [],
+  templateUrl: './blog-item-image.html',
+  styleUrl: './blog-item-image.scss',
+})
+export class BlogItemImage {
+
+}
