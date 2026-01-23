a5df1b5a695ebfc5256b27e47fcef9c3eb03fdbe
capSaymon <szymonpietruszka@onet.pl>
2026-01-23 14:08:25 +0100

    stworzenie komponentu add-post

---
+++ src/app/components/add-post/add-post.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'add-post',
+  imports: [],
+  templateUrl: './add-post.html',
+  styleUrl: './add-post.scss',
+})
+export class AddPost {
+
+}
