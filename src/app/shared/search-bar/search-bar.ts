68fdc5b496fefadfdda058321aadffce2ac61709
capSaymon <szymonpietruszka@onet.pl>
2026-01-23 23:05:40 +0100

    utworzenie w shared search bar

---
+++ src/app/shared/search-bar/search-bar.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+
+
+@Component({
+ selector: 'search-bar',
+ standalone: true,
+ imports: [FormsModule],
+ templateUrl: './search-bar.html',
+ styleUrl: './search-bar.scss'
+})
+export class SearchBarComponent implements OnInit {
+ public filterText: string = '';
+
+ @Output() name = new EventEmitter<string>();
+
+ constructor() {
+ }
+
+ ngOnInit(): void {
+ }
+
+ sendFilter(): void {
+   this.name.emit(this.filterText);
+ }
+}
