bc99d418a15f623ac3472b86073694655230f9da
capSaymon <szymonpietruszka@onet.pl>
2026-01-23 23:01:02 +0100

    aktualizacja blog item text

--- src/app/components/blog-item-text/blog-item-text.ts
+++ src/app/components/blog-item-text/blog-item-text.ts
@@ -1,12 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { SummaryPipe } from '../../pipes/summary-pipe';
+import { RouterModule } from "@angular/router";
 
 @Component({
   selector: 'blog-item-text',
-  imports: [SummaryPipe],
+  imports: [SummaryPipe, RouterModule],
   templateUrl: './blog-item-text.html',
   styleUrl: './blog-item-text.scss',
 })
 export class BlogItemText {
   @Input() text?: string;
+  @Input() id?: number | string;
 }
