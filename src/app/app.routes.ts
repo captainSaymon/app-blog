2bece557ca14aa16c30e4d82e10b7b0835659da4
capSaymon <szymonpietruszka@onet.pl>
2026-01-23 22:57:58 +0100

    przejście na nową ścieżkę

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 import { Blog } from './components/blog/blog';
+import { BlogItemDetails } from './components/blog-item-details/blog-item-details';
+
 
 export const routes: Routes = [
- {
-   path: '',
-   component: Blog,
- }
+   { path: '', component: Blog },
+   { path: 'blog/detail/:id', component: BlogItemDetails },
 ];
