a72f0ae693578b7293262174f04751601ec992f3
capSaymon <szymonpietruszka@onet.pl>
2026-01-23 22:55:36 +0100

    utworzenie komponentu blog item details

---
+++ src/app/components/blog-item-details/blog-item-details.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-item-details',
+  imports: [],
+  templateUrl: './blog-item-details.html',
+  styleUrl: './blog-item-details.scss',
+})
+export class BlogItemDetails {
+
+}
