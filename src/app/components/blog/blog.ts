9f7b08945682de34b5a34488ca76b9626a8e52a6
capSaymon <szymonpietruszka@onet.pl>
2026-01-23 23:19:23 +0100

    poprawka błedu

--- src/app/components/blog/blog.ts
+++ src/app/components/blog/blog.ts
@@ -1,20 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { CommonModule } from "@angular/common";
 import { DataService } from '../../services/data';
 import { BlogItem } from '../blog-item/blog-item';
 import { AddPost } from '../add-post/add-post';
 import { Gallery } from '../gallery/gallery';
+import { FilterTextPipe } from '../../pipes/filter-text-pipe';
 
 @Component({
  selector: 'blog',
  standalone: true,
- imports: [BlogItem, CommonModule, AddPost, Gallery],
+ imports: [BlogItem, CommonModule, AddPost, Gallery, FilterTextPipe],
  providers: [DataService],
  templateUrl: './blog.html',
  styleUrl: './blog.scss'
 })
 export class Blog implements OnInit{
  public items$: any;
+ @Input() filterText: string = '';
 
  constructor(private service: DataService) {
  }
