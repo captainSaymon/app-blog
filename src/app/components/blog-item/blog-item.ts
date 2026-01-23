452cb5a35a400b0f7716f9036b7176d0acebad57
capSaymon <szymonpietruszka@onet.pl>
2026-01-23 13:10:21 +0100

    dodanie komentarzy pod posty

--- src/app/components/blog-item/blog-item.ts
+++ src/app/components/blog-item/blog-item.ts
@@ -1,16 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { BlogItemImage } from '../blog-item-image/blog-item-image';
 import { BlogItemText } from '../blog-item-text/blog-item-text';
+import { CommentsSection } from '../comments-section/comments-section';
 
 @Component({
  selector: 'blog-item',
  standalone: true,
- imports: [BlogItemImage, BlogItemText],
+ imports: [BlogItemImage, BlogItemText, CommentsSection],
  templateUrl: './blog-item.html',
  styleUrl: './blog-item.scss'
 })
 export class BlogItem {
  @Input() image?: string;
  @Input() text?: string;
-
+ @Input() postId!: string;
 }
--- src/app/components/comments-section/comments-section.ts
+++ src/app/components/comments-section/comments-section.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CommentsService, Comment } from '../../services/comments-service';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-comments-section',
-  imports: [],
+  selector: 'comments-section',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './comments-section.html',
-  styleUrl: './comments-section.scss',
+  styleUrls: ['./comments-section.scss'],
 })
-export class CommentsSection {
+export class CommentsSection implements OnInit {
+  @Input() id!: string;
+  comments$!: Observable<Comment[]>;
 
+  constructor(private commentsService: CommentsService) {}
+
+  ngOnInit(): void {
+    this.comments$ = this.commentsService.getByPostId(this.id);
+  }
 }
