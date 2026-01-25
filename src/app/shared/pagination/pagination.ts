0f980b918f70e020f11513e109a44f1079fad724
capSaymon <szymonpietruszka@onet.pl>
2026-01-25 13:25:02 +0100

    aktualizacja pliku

--- src/app/shared/pagination/pagination.ts
+++ src/app/shared/pagination/pagination.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'app-pagination',
-  imports: [],
+  selector: 'pagination',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './pagination.html',
   styleUrl: './pagination.scss',
 })
-export class Pagination {
+export class PaginationComponent {
+  @Input() totalItems: number = 0;
+  @Input() itemsPerPage: number = 2;
+  @Input() currentPage: number = 1;
+  @Output() pageChange = new EventEmitter<number>();
 
+  get totalPages(): number {
+    return Math.ceil(this.totalItems / this.itemsPerPage);
+  }
+
+  get pages(): number[] {
+    return Array.from({ length: this.totalPages }, (_, i) => i + 1);
+  }
+
+  changePage(page: number): void {
+    if (page >= 1 && page <= this.totalPages) {
+      this.pageChange.emit(page);
+    }
+  }
 }
