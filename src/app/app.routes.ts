e0c3ed102a10fd9bb5dd2475ade4f12e5abaefbf
capSaymon <szymonpietruszka@onet.pl>
2026-01-27 22:00:21 +0100

    zabezpieczenie widoków za pomocą guardów

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,15 @@ import { Routes } from '@angular/router';
 import { BlogItemDetails } from './components/blog-item-details/blog-item-details';
 import { BlogHome } from './components/blog-home/blog-home';
 import { Home } from './components/home/home';
+import { authGuard } from './services/auth.guard';
 
 
 export const routes: Routes = [
    { path: '', component: Home },
-   { path: '', component: BlogHome },
+   { 
+      path: 'blog', 
+      component: BlogHome,
+	   canActivate: [authGuard] 
+   },
    { path: 'blog/detail/:id', component: BlogItemDetails },
 ];
