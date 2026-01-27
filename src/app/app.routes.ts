89ce34c7711432958f56c0a8e9d59c5479ba00e4
capSaymon <szymonpietruszka@onet.pl>
2026-01-27 21:26:21 +0100

    aktualizacja routingu aplikacji

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { BlogItemDetails } from './components/blog-item-details/blog-item-details';
 import { BlogHome } from './components/blog-home/blog-home';
+import { Home } from './components/home/home';
 
 
 export const routes: Routes = [
+   { path: '', component: Home },
    { path: '', component: BlogHome },
    { path: 'blog/detail/:id', component: BlogItemDetails },
 ];
