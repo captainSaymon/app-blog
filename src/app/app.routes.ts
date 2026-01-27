73e95ae54d980fc48c3ab3e53ce6fa2ccc197f3f
capSaymon <szymonpietruszka@onet.pl>
2026-01-27 22:25:14 +0100

    lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,31 @@
 import { Routes } from '@angular/router';
-import { BlogItemDetails } from './components/blog-item-details/blog-item-details';
-import { BlogHome } from './components/blog-home/blog-home';
-import { Home } from './components/home/home';
 import { authGuard } from './services/auth.guard';
-import { Login } from './components/login/login';
-import { Signup } from './components/signup/signup';
-
-
+ 
 export const routes: Routes = [
-   { path: '', component: Home },
-   { path: 'login', component: Login },
-   { path: 'signup', component: Signup },
-   { 
-      path: 'blog', 
-      component: BlogHome,
-	   canActivate: [authGuard] 
-   },
-   { path: 'blog/detail/:id', component: BlogItemDetails },
+  {
+	path: '',
+	loadComponent: () => import('./components/home/home')
+  	.then(m => m.Home)
+  },
+  {
+	path: 'blog',
+	loadComponent: () => import('./components/blog-home/blog-home')
+  	.then(m => m.BlogHome),
+	canActivate: [authGuard]
+  },
+  {
+	path: 'blog/detail/:id',
+	loadComponent: () => import('./components/blog-item-details/blog-item-details')
+  	.then(m => m.BlogItemDetails)
+  },
+  {
+	path: 'login',
+	loadComponent: () => import('./components/login/login')
+  	.then(m => m.Login)
+  },
+  {
+	path: 'signup',
+	loadComponent: () => import('./components/signup/signup')
+  	.then(m => m.Signup)
+  }
 ];
