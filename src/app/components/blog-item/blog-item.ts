3706874261015848657b9129a0dbddac8eab6cfe
capSaymon <szymonpietruszka@onet.pl>
2026-01-29 12:53:50 +0100

    polubienie posta lub rezygnacja z tego

--- src/app/components/blog-item/blog-item.ts
+++ src/app/components/blog-item/blog-item.ts
@@ -6,6 +6,7 @@ import { CommentsSection } from '../comments-section/comments-section';
 import { RouterModule } from '@angular/router';
 import { FavoritesService } from '../../services/favorites-service';
 import { Rating } from '../../shared/rating/rating';
+import { DataService } from '../../services/data-service';
 
 @Component({
   selector: 'blog-item',
@@ -18,8 +19,9 @@ export class BlogItem {
   @Input() image?: string;
   @Input() text?: string;
   @Input() postId!: string;
+  @Input() like!: number;
 
-  constructor(private favoritesService: FavoritesService) {}
+  constructor(private favoritesService: FavoritesService,  private dataService: DataService) {}
 
   get isFavorite(): boolean {
     return this.favoritesService.isFavorite(this.postId);
@@ -27,5 +29,28 @@ export class BlogItem {
 
   toggleFavorite(): void {
     this.favoritesService.toggleFavorite(this.postId);
+    if(this.favoritesService.isFavorite(this.postId))
+    {
+      this.like++;
+      this.dataService.addlike(this.postId).subscribe({
+        next: () => {
+          console.log('Polubienie posta');
+        },
+        error: (err) => {
+          console.error('Błąd polubienia posta', err);
+          this.like--;
+        }
+      });
+    }
+    else {
+      this.like--;
+      this.dataService.unlikePost(this.postId).subscribe({
+        next: () => console.log('Rezygnacja z polubienia posta'),
+        error: (err) => {
+          console.error('Błąd przy rezygnacji z polubienia posta', err);
+          this.like++;
+        }
+      });
+    }
   }
 }
